93f2531119818a85a19770d6644fde17dda7223b
lestec-al <alexyurgel@gmail.com>
2022-07-02 18:33:15 +0300

    Fix to slug creation

--- links/models.py
+++ links/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
+import random, string
 
 class Link(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="link", blank=True, null=True)
@@ -17,6 +18,13 @@ class Link(models.Model):
         return self.original
 
 def create_slug_from_id(instance, created, *args, **kwargs):
+    def test(slug):
+        qs = Link.objects.filter(slug=slug)
+        if qs.exists():
+            letters = string.ascii_lowercase
+            slug = ''.join(random.choice(letters) for _ in range(4))
+            slug = test(slug)
+        return slug
     if created:
         id = instance.id
         map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -24,7 +32,9 @@ def create_slug_from_id(instance, created, *args, **kwargs):
         while(int(id) > 0):
             shortURL += map[id % 62]
             id //= 62
-        instance.slug = shortURL[len(shortURL): : -1]
+        slug = shortURL[len(shortURL): : -1]
+        slug = test(slug)
+        instance.slug = slug
         instance.save()
 
 post_save.connect(receiver=create_slug_from_id, sender=Link)
