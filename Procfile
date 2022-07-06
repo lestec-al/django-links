ea954814fd8cd45eecff2bb1e5f8d5d4b2c4ff23
lestec-al <alexyurgel@gmail.com>
2022-07-06 20:36:42 +0300

    Deploying fixes

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn links_project.wsgi:application
+release: python manage.py migrate
--- links_project/settings.py
+++ links_project/settings.py
@@ -1,19 +1,26 @@
 from pathlib import Path
+import os
+from boto.s3.connection import S3Connection
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b$506shp'
+try:
+    SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
+except:
+    SECRET_KEY = "django-insecure-b$506shp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if SECRET_KEY == "django-insecure-b$506shp":
+    DEBUG = True
+    ALLOWED_HOSTS = []
+else:
+    DEBUG = False
+    ALLOWED_HOSTS = ["url-cuter.herokuapp.com"]
 
 
 # Application definition
@@ -39,6 +46,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+if DEBUG == False:
+    MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'csp.middleware.CSPMiddleware',
+    ]
 
 ROOT_URLCONF = 'links_project.urls'
 
@@ -68,6 +80,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    } if DEBUG == True else {
+        'ENGINE': S3Connection(os.environ['DATABASE_ENGINE']),
+        'NAME': S3Connection(os.environ['DATABASE_NAME']),
+        'USER': S3Connection(os.environ['DATABASE_USER']),
+        'PASSWORD': S3Connection(os.environ['DATABASE_PASSWORD']),
+        'HOST': S3Connection(os.environ['DATABASE_HOST']),
+        'PORT': S3Connection(os.environ['DATABASE_PORT']),
+        'CONN_MAX_AGE': S3Connection(os.environ['DATABASE_CONN_MAX_AGE']),
     }
 }
 
@@ -109,6 +129,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG == True:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+else:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_ROOT = os.path.join(BASE_DIR1, 'staticfiles')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR1, 'static'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -118,3 +149,46 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+# Deploying
+if DEBUG == False:
+
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+    CSP_DEFAULT_SRC = ("'self'", )
+
+    CSP_STYLE_SRC = ("'self'",
+        "'unsafe-inline'",
+        'cdn.jsdelivr.net',
+        'stackpath.bootstrapcdn.com')
+
+    CSP_SCRIPT_SRC = ("'self'",
+        'ajax.cloudflare.com',
+        'static.cloudflareinsights.com',
+        'www.google-analytics.com',
+        'ssl.google-analytics.com',
+        'cdn.ampproject.org',
+        'www.googletagservices.com',
+        'pagead2.googlesyndication.com',
+        'stackpath.bootstrapcdn.com',
+        'cdn.jsdelivr.net',
+        'code.jquery.com')
+
+    CSP_IMG_SRC = ("'self'",)
+
+    CSP_FONT_SRC = ("'self'", )
+    CSP_CONNECT_SRC = ("'self'",
+        'www.google-analytics.com' )
+    CSP_OBJECT_SRC = ("'self'", )
+    CSP_BASE_URI = ("'self'", )
+    CSP_FRAME_ANCESTORS = ("'self'", )
+    CSP_FORM_ACTION = ("'self'", )
+    CSP_INCLUDE_NONCE_IN = ('script-src', )
+    CSP_MANIFEST_SRC = ("'self'", )
+    CSP_WORKER_SRC = ("'self'", )
+    CSP_MEDIA_SRC = ("'self'", )
